1a8675f276838e6a67dc60bda875b98ce9a74ad4
Sharfras aqsan <sharfrasaqsan@gmail.com>
2022-09-05 23:09:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [temperature, setTemperature] = useState(0);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    const newTemperature = temperature + 1;
+    if (newTemperature > 20) {
+      setTemperatureColor("hot");
+    }
+    setTemperature(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    const newTemperature = temperature - 1;
+    if (newTemperature < 20) {
+      setTemperatureColor("cold");
+    }
+    setTemperature(newTemperature);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="card text-center">
+        <div className="card-body">
+          <h1>Temperature Control</h1>
+        </div>
+        <div className="my-2">
+          <h2>Current Temperature</h2>
+          <h3 className={`display-temperature ${temperatureColor}`}>
+            {temperature}°C
+          </h3>
+          <button
+            className="btn btn-primary mx-3"
+            onClick={increaseTemperature}
+          >
+            +
+          </button>
+          <button className="btn btn-danger mx-3" onClick={decreaseTemperature}>
+            -
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
